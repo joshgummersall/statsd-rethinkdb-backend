d9f7bae4d2bdf19a4e1100b0fd0ebe701b8577b2
Josh Gummersall <josh@joshgummersall.me>
2016-02-29 14:22:36 -0800

    Translate codebase to es2015

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var Rethinkdb = require('./lib/rethinkdb');
-
-exports.init = function(startupTime, config, events) {
-  var rethinkInstance = new Rethinkdb(startupTime, config, events);
-  return true;
-};
